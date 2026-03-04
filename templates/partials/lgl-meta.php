d5d20f277986e736d21a3353fadf3006dd3ef4ac
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-04 16:24:40 +0800

    cs

--- templates/partials/lgl-meta.php
+++ templates/partials/lgl-meta.php
@@ -1,14 +1,28 @@
 <?php
 $listing_fields = LGL_Shortcodes::get_external_listing_fields();
 
+// Define an array of meta keys you want to exclude from the frontend display.
+// Populate this array with the exact keys defined in get_listing_detail_fields() (e.g., 'price', 'year', 'internal_stock_number').
+$exclude_keys = array( 'internal_stock_number', 'rrp', 'warranty', 'feature', 'extras' ); 
+
 if (!empty($listing_fields)) {
     // Access the specific field groupings
     $common_fields = $listing_fields['common'];
     $motorhome_campervan_fields = $listing_fields['motorhome_campervan'];
+    
     echo "<div class='lgl-meta-list'>";
+    
     // Example iteration over common fields
     foreach ($common_fields as $meta_key => $label) {
+        
+        // Intercept and skip the current iteration if the meta key exists in the exclusion array.
+        // The 3rd parameter 'true' ensures strict type checking.
+        if (in_array($meta_key, $exclude_keys, true)) {
+            continue;
+        }
+
         $meta_value = get_post_meta($post_id, $meta_key, true);
+        
         if (!empty($meta_value)) {
             echo "<div class='lgl-meta-item lgl-{$meta_key}'>";
             echo "<span class='lgl-meta-icon-label'>";
@@ -26,55 +40,4 @@ if (!empty($listing_fields)) {
     }
     echo "</div>";
 }
-$schema =  [
-    'condition' => [
-        'class'    => 'lgl-condition',
-        'label'    => 'Condition',
-        'meta_key' => 'condition',
-        'svg'      => '<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.66602 18.4166H17.3327M8.66602 18.4166C8.66602 19.6133 7.69597 20.5833 6.49935 20.5833C5.30273 20.5833 4.33268 19.6133 4.33268 18.4166M8.66602 18.4166C8.66602 17.22 7.69597 16.25 6.49935 16.25C5.30273 16.25 4.33268 17.22 4.33268 18.4166M17.3327 18.4166C17.3327 19.6133 18.3027 20.5833 19.4993 20.5833C20.696 20.5833 21.666 19.6133 21.666 18.4166M17.3327 18.4166C17.3327 17.22 18.3027 16.25 19.4993 16.25C20.696 16.25 21.666 17.22 21.666 18.4166M4.33268 18.4166H3.89935C3.29263 18.4166 2.98926 18.4166 2.75753 18.2985C2.55369 18.1947 2.38796 18.029 2.28409 17.8251C2.16602 17.5934 2.16602 17.2901 2.16602 16.6833V15.3833C2.16602 14.1699 2.16602 13.5631 2.40217 13.0996C2.60989 12.692 2.94135 12.3605 3.34904 12.1528C3.81251 11.9166 4.41924 11.9166 5.63268 11.9166H18.6327C19.4378 11.9166 19.8404 11.9166 20.1772 11.9699C22.0314 12.2636 23.4857 13.7179 23.7794 15.5721C23.8327 15.9089 23.8327 16.3115 23.8327 17.1166C23.8327 17.3179 23.8327 17.4186 23.8194 17.5027C23.7459 17.9663 23.3823 18.3298 22.9188 18.4033C22.8346 18.4166 22.734 18.4166 22.5327 18.4166H21.666M10.8327 5.41663V11.9166M4.33268 11.9166L4.69183 9.76175C4.94911 8.21805 5.07776 7.4462 5.46292 6.86699C5.80245 6.35642 6.27949 5.9523 6.83893 5.70134C7.47359 5.41663 8.25608 5.41663 9.82108 5.41663H13.4664C14.4838 5.41663 14.9926 5.41663 15.4544 5.55686C15.8632 5.68102 16.2435 5.88456 16.5736 6.15585C16.9465 6.46229 17.2287 6.88559 17.7931 7.73219L20.5827 11.9166" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
-    ],
-    'year' => [
-        'class'    => 'lgl-year',
-        'label'    => 'Year',
-        'meta_key' => 'year',
-        'svg'      => '<svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false"> <rect width="17" height="16" fill="url(#pattern0_461_3)"></rect> <defs> <pattern id="pattern0_461_3" patternContentUnits="objectBoundingBox" width="1" height="1"> <use xlink:href="#image0_461_3" transform="scale(0.0294118 0.03125)"></use> </pattern> <image id="image0_461_3" width="34" height="32" preserveAspectRatio="none" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAgCAYAAAB3j6rJAAAABHNCSVQICAgIfAhkiAAAAb9JREFUWEftWL1OwlAUPqeEONrVTWmNxkmfAN5A3Ftk1UUmHcVRJ110FegD6BNYnkAmo7Ho6CiOBuF47i1YpJaSok1M7hmatOfvu19PTvlAiLKctQOI5+zuAtAatB0vMnaSw7BMALzjkCwQ7cKTc/FTOEbWMOx99h1Lf59W4dl5SARkyVoBDe8HuQfQbpxMBmLaeW64zkG6DERYYEayPhB4ZFbmEgEBfAcNlmUuURcIXgZ1OgywBV6j6bdbLOugfdxwUwEiMMQj8OpV+cCwXb7mkwGBJrNQkLlmqcpgDsfquNDLbCEY1iXj2Q43wTNo1yvyec66DQGdFhVRi+diwz9Q6ZRp2QunUo2B2K/s8F9H2Fw+gZ4YxLCeAIPY4VufmbB1BBCa9nB/GaeAjLOrGIljRC6XFO1rN428Gqrx96ScIgjeK8EOC4CMbtK00IxsWgVEkq4YGZ89xYhiJG4fqRlRM/K/ZwTgmvVHMe4Qv+o37Cuutylqiq+v+Jk/LxsQVaT6SsOEqkRknSPtbYLASgPNsIcQWEJyZnqCoqSSclbETZacxeDfANMqQJ/1r4ZRqm/Wht/z+8QinBWg57jC8QnCkuRI6X88sAAAAABJRU5ErkJggg=="></image> </defs> </svg>'
-    ],
-
-    'internal_stock_number' => [
-        'class'    => 'lgl-internal-stock-number',
-        'label'    => 'Internal Stock Number',
-        'meta_key' => 'internal_stock_number',
-        'svg'      => '<svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false"> <rect width="17" height="16" fill="url(#pattern0_461_3)"></rect> <defs> <pattern id="pattern0_461_3" patternContentUnits="objectBoundingBox" width="1" height="1"> <use xlink:href="#image0_461_3" transform="scale(0.0294118 0.03125)"></use> </pattern> <image id="image0_461_3" width="34" height="32" preserveAspectRatio="none" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAgCAYAAAB3j6rJAAAABHNCSVQICAgIfAhkiAAAAb9JREFUWEftWL1OwlAUPqeEONrVTWmNxkmfAN5A3Ftk1UUmHcVRJ110FegD6BNYnkAmo7Ho6CiOBuF47i1YpJaSok1M7hmatOfvu19PTvlAiLKctQOI5+zuAtAatB0vMnaSw7BMALzjkCwQ7cKTc/FTOEbWMOx99h1Lf59W4dl5SARkyVoBDe8HuQfQbpxMBmLaeW64zkG6DERYYEayPhB4ZFbmEgEBfAcNlmUuURcIXgZ1OgywBV6j6bdbLOugfdxwUwEiMMQj8OpV+cCwXb7mkwGBJrNQkLlmqcpgDsfquNDLbCEY1iXj2Q43wTNo1yvyec66DQGdFhVRi+diwz9Q6ZRp2QunUo2B2K/s8F9H2Fw+gZ4YxLCeAIPY4VufmbB1BBCa9nB/GaeAjLOrGIljRC6XFO1rN428Gqrx96ScIgjeK8EOC4CMbtK00IxsWgVEkq4YGZ89xYhiJG4fqRlRM/K/ZwTgmvVHMe4Qv+o37Cuutylqiq+v+Jk/LxsQVaT6SsOEqkRknSPtbYLASgPNsIcQWEJyZnqCoqSSclbETZacxeDfANMqQJ/1r4ZRqm/Wht/z+8QinBWg57jC8QnCkuRI6X88sAAAAABJRU5ErkJggg=="></image> </defs> </svg>'
-    ],
-
-
-
-];
-$output = '<div class="lgl-content-ss lgl-meta-list">';
-$has_items = false;
-
-foreach ($schema as $key => $data) {
-    // Fetch the custom field data
-    $meta_value = get_post_meta($post_id, $data['meta_key'], true);
-
-    // Strict validation: Only proceed if there is data and it isn't a placeholder
-    if (! empty($meta_value) && trim($meta_value) !== 'N/A') {
-        $has_items = true;
-
-        $output .= sprintf(
-            '<div class="lgl-meta-item %1$s">
-            <span class="lgl-meta-icon-label">
-                    %2$s
-                    <span class="lgl-label">%3$s</span></span><span class="lgl-value">%4$s</span>
-                </div>',
-            esc_attr($data['class']),
-            $data['svg'], // SVGs are hardcoded in the trusted schema, safe to output directly
-            esc_html($data['label']),
-            esc_html($meta_value)
-        );
-    }
-}
-
-$output .= '</div>';
-
-echo $output;
+?>
