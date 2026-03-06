392d22ac5520108be52c14d8b4eb0fd361606110
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-06 23:40:38 +0800

    Update lgl_listing_grid.php

--- templates/shortcodes/lgl_listing_grid.php
+++ templates/shortcodes/lgl_listing_grid.php
@@ -1,9 +1,11 @@
 <?php
 if ($post_type) {
     echo $post_type;
-    echo LGL_Shortcodes::get_search_results_data(
+    $result = LGL_Shortcodes::get_search_results_data(
         post_type: $post_type,
         paged: 1,
         posts_per_page: 6
     );
+
+    eho $result['html'];
 }
