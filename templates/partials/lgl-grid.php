58a4defb68d21073fdb0e2d326c0a372a794b29a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-07 04:47:26 +0800

    Update lgl-grid.php

--- templates/partials/lgl-grid.php
+++ templates/partials/lgl-grid.php
@@ -17,6 +17,7 @@ $year = get_post_meta($post_id, 'year', true);
 $axles = get_post_meta($post_id, 'axles', true);
 $link = get_the_permalink();
 $title = get_the_title();
+$lgl_options = get_option('lgl_settings', array());
 $disable_wishlist = !empty($lgl_options['disable_wishlist']);
 $disable_compare  = !empty($lgl_options['disable_compare']);
 ?>
