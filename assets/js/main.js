2faa2be77feb381cc1703ef09641316b829e72af
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-11 20:13:53 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -393,13 +393,14 @@
     function vehicle_slider() {
         //vehicle slider
         $('.vehicle-slider-js').slick({
+            mobileFirst: true, // Reverses default max-width breakpoint calculation to min-width
             slidesToShow: 1,
             slidesToScroll: 1,
             fade: false, // Disabled to allow standard inline block layout for multiple slides
             arrows: true,
             responsive: [
-
                 {
+                    // Triggers at min-width: 768px
                     breakpoint: 767,
                     settings: {
                         slidesToShow: 2,
@@ -407,13 +408,13 @@
                     }
                 },
                 {
-                    breakpoint: 1025,
+                    // Triggers at min-width: 1025px
+                    breakpoint: 1024,
                     settings: {
                         slidesToShow: 3,
                         slidesToScroll: 3,
                     }
                 }
-
             ],
             prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/> </svg></button>',
             nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/> </svg></button>'
