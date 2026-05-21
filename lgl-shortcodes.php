99ce8be60a8f1d2115feb9b013029a7a98a83935
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-21 22:04:15 +0800

    Update lgl-shortcodes.php

--- lgl-shortcodes.php
+++ lgl-shortcodes.php
@@ -1101,7 +1101,7 @@ if (! class_exists('LGL_Shortcodes')) {
         public function iframe_resizer()
         {
         ?>
-            <script src="http://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.6.0/iframeResizer.min.js"></script>
+          <script src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.6.0/iframeResizer.min.js"></script>
             <script>
                 iFrameResize({
                     log: false,
