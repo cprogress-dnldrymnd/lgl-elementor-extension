13980c68a26a8ebf3ffadb0bc8c074436568ebad
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 02:45:46 +0800

    Update single-lgl.php

--- templates/single-lgl.php
+++ templates/single-lgl.php
@@ -111,8 +111,12 @@ $all_image_ids = array_values(array_unique(array_filter($all_image_ids)));
                                         </div>
                                     <?php } ?>
                                 </div>
-                                <div class="swiper-button-next"></div>
-                                <div class="swiper-button-prev"></div>
+                                <div class="swiper-button-next">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"></path> </svg>
+                                </div>
+                                <div class="swiper-button-prev">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"></path> </svg>
+                                </div>
                             </div>
 
                             <div class="swiper js-gallery-slider-nav lgl-gallery-slider lgl-slider-nav" style="margin-top: 10px;">
